b0a39f42168ddfed32811a7546234273a4de187e
Conrad Hope <conjhope@outlook.com>
2023-08-18 17:59:55 -0600

    last commit

---
+++ week10assignment.js
@@ -0,0 +1,54 @@
+// let myForm = document.createElement('form');
+// let myNameLabel= document.createElement('label');
+// let myNameInput = document.createElement('input');
+
+// myNameLabel.innerHTML = 'Name';
+
+// myForm.append(myNameLabel);
+// myForm.append(myNameInput);
+
+// document.body.append(myForm);
+
+// let mySuperpowerLabel = document.createElement('label');
+// let mySuperpowerInput = document.createElement('input');
+
+// mySuperpowerLabel.innerHTML = 'Superpower';
+
+// mySubmitButton = document.createElement('button');
+
+// mySubmitButton.innerHTML = 'Submit';
+
+// myForm.append(mySuperpowerLabel);
+// myForm.append(mySuperpowerInput);
+
+// myForm.append(mySubmitButton);
+
+// console.log(myForm);
+
+// myForm.style.backgroundColor = 'purple';
+
+// myForm.style.color = 'white';
+
+// myForm.style.display = 'flex';
+
+// myForm.style.flexDirection = 'column';
+
+// myForm.style.maxWidth = '250px';
+
+// let myTable = document.createElement('table');
+// let myTr = document.createElement('tr');
+// let myTdName = document.createElement('td');
+// let myTdEmail = document.createElement('td');
+
+// myTdName.innerHTML = 'Name';
+// myTdEmail.innerHTML = 'Email';
+
+// myTable.append(myTr);
+// myTr.append(myTdName);
+// myTr.append(myTdEmail);
+
+// document.body.append(myTable);
+
+// myTable.style.border = '1px solid black';
+
+// console.log(myTable);
